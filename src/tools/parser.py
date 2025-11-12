43a009cb5a8d8261841e5d05f385a256f79e8f2a
Rafael Castilho Carvalho <rafacien.rc@gmail.com>
2025-11-12 11:58:09 -0400

    Starting directories

---
+++ src/tools/parser.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+def parse_planilha(file_path):
+    """
+    Parses an Excel file and returns a DataFrame.
+
+    Parameters:
+    file_path (str): The path to the Excel file.
+
+    Returns:
+    pd.DataFrame: The parsed data as a DataFrame.
+    """
+    try:
+        colunas = ['Data','Horário Inicial','Horário Final','Descrição de Serviços','Endereço','Bairro','Zona','Técnico','Status','Uso Mútuo']
+        colunas2 = ['Data','Horário Inicial','Horário Final','Endereço','Bairro','Zona','Técnico','Descrição de Serviços','Uso Mútuo','Status']
+
+        df = pd.read_excel(file_path, usecols = colunas)
+        df  = df[colunas2]
+
+        return df
+    except Exception as e:
+        print(f"Error reading the Excel file: {e}")
+        return None
+
+if __name__ == "__main__":
+    file_path = "./planilha/Cronograma_de_Linha_Morta_25-11-07.xlsx"
+    df = parse_planilha(file_path)
+    if df is not None:
+        print(df.head())
